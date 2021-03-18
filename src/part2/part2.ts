e01e4a607d44a871467ed6744c4ccac19ea4d21c
abdalla1611 <abdalla@post.bgu.ac.il>
2021-03-19 00:00:22 +0200

    install npm

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -3,10 +3,11 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countVowels = undefined;
+export const countVowels = stringToArray(str).reduce((count, letter) => count + 'aeiouAEIOU'.includes(letter), 0);
 
 /* Question 2 */
 export const runLengthEncoding = undefined;
 
 /* Question 3 */
 export const isPaired = undefined;
+
