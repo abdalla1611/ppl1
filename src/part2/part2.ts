ef32ee52441bb86b653fa4b371f6f972d9a81b3f
abdalla1611 <abdalla@post.bgu.ac.il>
2021-03-19 20:05:17 +0200

    Q1

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -3,11 +3,10 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countVowels = stringToArray(str).reduce((count, letter) => count + 'aeiouAEIOU'.includes(letter), 0);
+export const countVowels = (str : string): number => stringToArray(str).filter(chstr=>"aieouAIEOU".includes(chstr)==true).reduce((acc,curr)=>acc+1,0);
 
 /* Question 2 */
 export const runLengthEncoding = undefined;
 
 /* Question 3 */
 export const isPaired = undefined;
-
