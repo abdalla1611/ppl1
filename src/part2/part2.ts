580fee60d9a9e8fc5a96f652428ebca596cb538e
abdalla1611 <abdalla@post.bgu.ac.il>
2021-03-19 20:13:14 +0200

    test3

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -6,7 +6,7 @@ const stringToArray = R.split("");
 export const countVowels = (str : string): number => stringToArray(str).filter(chstr=>"aieouAIEOU".includes(chstr)==true).reduce((acc,curr)=>acc+1,0);
 
 /* Question 2 */
-export const runLengthEncoding = undefined;
+export const runLengthEncoding = abdalla;
 
 /* Question 3 */
 export const isPaired = undefined;
