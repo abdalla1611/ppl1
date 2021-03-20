1a8fdcf745acfb59a6341e8256632c658b533bee
abdalla1611 <abdalla@post.bgu.ac.il>
2021-03-20 19:53:28 +0200

    Q2

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -6,7 +6,33 @@ const stringToArray = R.split("");
 export const countVowels = (str : string): number => stringToArray(str).filter(chstr=>"aieouAIEOU".includes(chstr)==true).reduce((acc,curr)=>acc+1,0);
 
 /* Question 2 */
-export const runLengthEncoding = abdalla;
+export const runLengthEncoding = (str : string) =>{
+    const arr =stringToArray(str);
+    const output =arr.reduce(function(acc :[string,string,number] ,curr,index :number){
+
+        if(arr[index]==arr[index+1]){
+            acc[2]=acc[2]+1;
+            return acc;
+        }
+        else{
+            if(acc[2]==1){
+                acc[0]= `${acc[0]}${acc[1]}`;
+            }
+            else {
+                acc[0]= `${acc[0]}${acc[1]}${acc[2]}`;
+                acc[1]= arr[index+1];
+                acc[2]=1 ;
+            }
+            return acc ;
+        }
+        
+    },["",arr[0],1])
+
+    return output[0] ;
+}
+
+console.log(countVowels("aaaabbaa"));
+console.log(runLengthEncoding("aaabbaa"));
 
 /* Question 3 */
-export const isPaired = undefined;
+export const isPaired = undefined ;
