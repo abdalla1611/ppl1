945137c97b95230e5103c999e0e05e3f937c0cb3
abdalla1611 <abdalla@post.bgu.ac.il>
2021-03-20 22:33:54 +0200

    part2_Q2_updated

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -8,30 +8,29 @@ export const countVowels = (str : string): number => stringToArray(str).filter(c
 /* Question 2 */
 export const runLengthEncoding = (str : string) =>{
     const arr =stringToArray(str);
-    const output =arr.reduce(function(acc :[string,string,number] ,curr,index :number){
+    const output =arr.reduce(function(acc :[string,number] ,curr,index :number){
 
         if(arr[index]==arr[index+1]){
-            acc[2]=acc[2]+1;
-            return acc;
+            acc[1]=acc[1]+1;
+            
         }
         else{
-            if(acc[2]==1){
-                acc[0]= `${acc[0]}${acc[1]}`;
+            if(acc[1]==1){
+                acc[0]= `${acc[0]}${arr[index]}`;
             }
             else {
-                acc[0]= `${acc[0]}${acc[1]}${acc[2]}`;
-                acc[1]= arr[index+1];
-                acc[2]=1 ;
+                acc[0]= `${acc[0]}${arr[index]}${acc[1]}`;
+                acc[1]=1 ;
             }
-            return acc ;
+            
         }
+        return acc;
         
-    },["",arr[0],1])
+    },["",1])
 
     return output[0] ;
 }
 
-console.log(countVowels("aaaabbaa"));
 console.log(runLengthEncoding("aaabbaa"));
 
 /* Question 3 */
