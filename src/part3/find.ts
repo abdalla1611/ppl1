470628e4a124ac828e609d10ed5d80d9c2d30437
abdalla1611 <abdalla@post.bgu.ac.il>
2021-03-21 18:52:23 +0200

    find.ts_sol

--- src/part3/find.ts
+++ src/part3/find.ts
@@ -8,7 +8,12 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
     throw "No element found.";
 }
 
-export const findResult = undefined;
+export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => {
+    for (let i = 0; i < a.length; i++) {
+    if (pred(a[i])) return makeOk(a[i]);
+    }
+    return makeFailure(" no such element exists");
+    };
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
@@ -20,6 +25,11 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
     }
 }
 
-export const returnSquaredIfFoundEven_v2 = undefined;
+export const returnSquaredIfFoundEven_v2 = (a: number[]):Result<number>=>{
+        return bind(findResult(x=>x%2==0,a),x=>makeOk(x*x));
+} ;
 
-export const returnSquaredIfFoundEven_v3 = undefined;
+export const returnSquaredIfFoundEven_v3 = (a: number[]):number=>{
+    return either(findResult(x=>x%2==0,a),x=>x*x,str=>-1);
+};
+
